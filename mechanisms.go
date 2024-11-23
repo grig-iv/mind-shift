8d8989387100db4cdfc8a8df98e3093ebeee4888
grig-iv <abstractgrig@gmail.com>
2024-11-23 15:22:39 +0300

    change NetWMState on fullscreen change

--- mechanisms.go
+++ mechanisms.go
@@ -4,10 +4,17 @@ import (
 	"log"
 
 	"github.com/grig-iv/mind-shift/x"
+	"github.com/jezek/xgb/xproto"
 )
 
 func (wm *windowManager) enableFullscreen(client *client) {
 	log.Println("Enabling fullscreen. client:", client.window)
+	x.ChangeProperty32(
+		client.window,
+		x.AtomOrNone(x.NetWMState),
+		xproto.AtomAtom,
+		uint(x.AtomOrNone(x.NetWMFullscreen)),
+	)
 	client.isFullscreen = true
 	x.ChangeBorderWidth(client.window, 0)
 	x.Raise(client.window)
@@ -16,6 +23,12 @@ func (wm *windowManager) enableFullscreen(client *client) {
 
 func (wm *windowManager) disableFullscreen(client *client) {
 	log.Println("Disabling fullscreen. client:", client.window)
+	x.ChangeProperty32(
+		client.window,
+		x.AtomOrNone(x.NetWMState),
+		xproto.AtomAtom,
+		0,
+	)
 	client.isFullscreen = false
 	x.ChangeBorderWidth(client.window, borderWidth)
 	wm.view(wm.currTag)
